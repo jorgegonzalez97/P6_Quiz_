6f82bc6949a2f735be62f8cd27460f74c6f18b5c
Jorge Gonzalez Lopez <jorge.gonzalezl@alumnos.upm.es>
2018-05-18 15:50:11 +0200

    p6

---
+++ models/index.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+// Load ORM
+const Sequelize = require('sequelize');
+
+
+// To use SQLite data base:
+//    DATABASE_URL = sqlite:quiz.sqlite
+// To use  Heroku Postgres data base:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
+
+const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite";
+
+const sequelize = new Sequelize(url);
+
+// Import the definition of the Quiz Table from quiz.js
+sequelize.import(path.join(__dirname, 'quiz'));
+
+// Session
+sequelize.import(path.join(__dirname,'session'));
+
+// Create tables
+sequelize.sync()
+    .then(function(){
+        sequelize.models.quiz.count();
+    })
+    .then(function (count) {
+        if(!count){
+            return sequelize.models.quiz.bulkCreate([
+                { question: "Capital de Italia", answer: "Roma"},
+                { question: "Capital de Francia", answer: "París"},
+                { question: "Capital de España", answer: "Madrid"},
+                { question: "Capital de Portugal", answer: "Lisboa"}
+            ]);
+        }
+    })
+    .then(() => console.log('Data Bases created successfully'))
+    .catch(error => {
+        console.log("Error creating the data base tables:", error);
+        process.exit(1);
+    });
+
+
+module.exports = sequelize;
